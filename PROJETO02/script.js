1e1af28464f13d24317f9a380f5910be9fa57a9b
Felipe Calais <felipe_calais@hotmail.com>
2022-10-29 15:25:26 -0300

    Novo projeto- Projeto 02

---
+++ PROJETO02/script.js
@@ -0,0 +1,16 @@
+//Selecionando elementos
+
+
+const prevop = document.getElementById('previous-operation')  
+const currtop = document.getElementById('current-operation')
+
+const buttons = document.querySelectorAll(".container-calc button") //Seleciona todos os botões 
+
+
+//Lógica do programa
+class calculator {
+
+}
+
+
+//
