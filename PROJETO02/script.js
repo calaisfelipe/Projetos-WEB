82122dfd5c491dc1ec793ba88c8607b7df3759c5
Felipe Calais <felipe_calais@hotmail.com>
2022-10-31 00:17:01 -0300

    Atualização

--- PROJETO02/script.js
+++ PROJETO02/script.js
@@ -9,8 +9,53 @@ const buttons = document.querySelectorAll(".container-calc button") //Seleciona 
 
 //Lógica do programa
 class calculator {
+    constructor(prevop, currtop){
+        this.prevop = prevop;
+        this.currtop = currtop;
+        this.currentOperation = "";
+    }
+
+
+    // Adiciona um digito na tela da calculadora
+    addDigit(digit) {
+
+        //Verifica se já tem um ponto e não deixa colocar mais um
+        if (digit === "." &&  this.currtop.innerText.includes(".")){
+            return;
+        }
+
+
+
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+
+
+
+    
+    //Mudas os valores na tela da calculadora
+    updateScreen(){
+        this.currtop.innerText += this.currentOperation 
+    }
 
 }
 
+const calc = new calculator(prevop, currtop)
 
-//
+
+// Adicionando evento nos botões
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {   //evento click 
+        const value = e.target.innerText; // evento de imprimir o valor de texto 
+
+        if(+value >=0 || value ==="."){   //diferencia quando digita um numero ou 
+                                            // uma operação na calculadora
+            calc.addDigit(value)
+        } else {
+            console.log("op:"+value)
+        }
+
+    })
+})
