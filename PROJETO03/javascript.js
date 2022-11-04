4047b365be6c1bb9556a62e16ec3497863a5efe7
Felipe Calais <felipe_calais@hotmail.com>
2022-11-04 01:06:08 -0300

    Conclusão Projeto03

---
+++ PROJETO03/javascript.js
@@ -0,0 +1,41 @@
+//SELETORES
+var btntask = document.querySelector('#btnadd')
+var list = document.querySelector('#lista')
+var newtask = document.querySelector('#itask')
+
+
+btntask.addEventListener('click',function(){
+    let nometask = newtask.value
+    
+    if(newtask.value != ''){
+    let task = document.createElement('li')
+    list.appendChild(task)
+    task.innerText = nometask
+
+    let taskbtndel = document.createElement('button')
+    task.appendChild(taskbtndel)
+    taskbtndel.id = "btndel"
+    taskbtndel.textContent = `X`
+
+    let taskbtnconfirm = document.createElement('button')
+    task.appendChild(taskbtnconfirm)
+    taskbtnconfirm.id = "btnok"
+    taskbtnconfirm.textContent = `\u{2714}`
+
+    newtask.focus()
+    newtask.value=""
+
+    taskbtnconfirm.addEventListener('click',function(){
+        if(task.style.backgroundColor ===''){
+            task.style.backgroundColor = 'lightgreen'}
+            else {task.style.backgroundColor = ''}
+    })
+
+    taskbtndel.addEventListener('click',function(){
+        task.remove()
+    })} 
+    
+    else{ alert('Insira uma tarefa')}
+
+})
+
