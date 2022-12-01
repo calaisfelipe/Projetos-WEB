cffee7995d3242bada0937b6081688b66e38fea2
Felipe Calais <felipe_calais@hotmail.com>
2022-12-01 13:15:51 -0300

    Atualização do projeto

--- PROJETO06/script.js
+++ PROJETO06/script.js
@@ -1,35 +1,71 @@
-/*
 
-box-shadow: inset    3px   3px  3px   1px  #000000 
-            interno? hor  vert  blur  spread  color 
-
-opacity: 1;
-
-    let boxShadow {
-        inset:true/false,
-        horizontal: 3,
-        vertical:3,
-        blur:3,
-        spread:1,
-        color: '#000000',
-        opacity: 1
-    }
-
-
-*/
 
 class BoxShadowGenerator{
-    constructor(sombraInterna,horizontal,vertical,blur,spread,color,opacity){
-        this.sombraInterna = sombraInterna
+    constructor(horizontal,txtHorizontal,vertical,txtVertical,blur,txtBlur,spread,txtSpread,previewBox,rule,webkitRule,mozRule){
         this.horizontal = horizontal
+        this.txtHorizontal = txtHorizontal
         this.vertical = vertical
+        this.txtVertical = txtVertical
         this.blur = blur
+        this.txtBlur = txtBlur
         this.spread = spread
-        this.color = color
-        this.opacity = opacity
+        this.txtSpread = txtSpread
+        this.previewBox = previewBox
 
+        this.rule = rule
+        this.webkitRule = webkitRule
+        this.mozRule = mozRule
+    }
+
+    // neWshadowBox.inputRef = input.value
+
+    showValuesInput(){
+       this.txtHorizontal.value = this.horizontal.value
+       this.txtVertical.value = this.vertical.value
+       this.txtBlur.value = this.blur.value
+       this.txtSpread.value = this.spread.value
+
+       this.ApplyRule()
+       this.showRule()
+    }
+
+    ApplyRule(){
+        this.previewBox.style.boxShadow = `${this.txtHorizontal.value}px ${this.txtVertical.value}px ${this.txtBlur.value}px ${this.txtSpread.value}px #000`
+
+        this.currentRule = this.previewBox.style.boxShadow
 
     }
+
+    showRule(){
+        this.rule.textContent = this.currentRule
+        this.webkitRule.textContent = this.currentRule
+        this.mozRule.textContent = this.currentRule
+    }
+
+
+    updateValue(type, value){
+       switch (type){
+        case "horizontal":
+            this.txtHorizontal.value = value
+            break
+        case "vertical":
+            this.txtVertical.value = value
+            break
+        case "blur":
+            this.txtBlur.value = value
+            break
+        case "spread":
+            this.txtSpread.value = value
+            break
+       }
+       this.ApplyRule()
+       this.showRule()
+    }
+
+    updateColor(){
+
+    }
+
 }
 
 //seletores
@@ -60,4 +96,64 @@ const rule = document.querySelector('#rule span')
 const webkitRule = document.querySelector('#webkit-rule span')
 const mozRule = document.querySelector('#moz-rule span')
 
+
+//Inicio do programa
+ const neWshadowBox = new BoxShadowGenerator(
+    horizontal,
+    txtHorizontal,
+    vertical,
+    txtVertical,
+    blur,
+    txtBlur,
+    spread,
+    txtSpread,
+    previewBox,
+    rule,
+    webkitRule,
+    mozRule) 
+
+
+    neWshadowBox.showValuesInput()
+    console.log(neWshadowBox.horizontal.value)    
+
 //eventos
+
+
+horizontal.addEventListener('input', (e) =>{
+    const value = e.target.value      
+    neWshadowBox.updateValue("horizontal", value)
+ 
+})
+
+vertical.addEventListener('input', (e) =>{
+    const value = e.target.value  
+    neWshadowBox.updateValue("vertical", value)
+ 
+})
+
+blur.addEventListener('input', (e) =>{
+    const value = e.target.value  
+    neWshadowBox.updateValue("blur", value)
+ 
+})
+
+spread.addEventListener('input', (e) =>{
+    const value = e.target.value  
+    neWshadowBox.updateValue("spread", value)
+ 
+})
+
+
+
+/*
+//TENTATIVA COM FOREACH
+[horizontal,vertical,blur,spread].forEach(el => {
+    el.addEventListener('input', (e)=>{
+        const value = e.target.value
+        
+        neWshadowBox.updateValue(el, value)
+    })
+    
+});*/
+
+
